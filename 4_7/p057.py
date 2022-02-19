c1d7d460d8d165ae88f5f64ba679d576fc5ad57a
Shuhei Abe <blueletter123456789@gmail.com>
2022-02-19 10:14:27 +0900

    solved 057

--- 4_7/p057.py
+++ 4_7/p057.py
@@ -1,8 +1,35 @@
 def new_list(n):
     return [[0]*n for _ in range(n)]
 
+
+def div_matrix(a, b, m):
+    ans = new_list(len(a))
+    for i in range(len(a)):
+        for j in range(len(a)):
+            for k in range(len(a)):
+                ans[i][j] += a[i][k] * b[k][j]
+                ans[i][j] %= m
+    return ans
+    
+
+def bin_pow(r, e, m):
+    res = r[:]
+    flg = True
+    for i in range(65):
+        if e & 1 << i:
+            if flg:
+                res = r[:]
+                flg = False
+            else:
+                res = div_matrix(res, r, m)
+        r = div_matrix(r, r, m)
+    return res
+
+
 k, n = map(int, input().split())
-r = new_list(2**k)
+e = 2**k
+r = new_list(e)
+M = 1000000007
 if k == 2:
     r[0][3] = 1
     r[1][2] = 1
@@ -34,4 +61,5 @@ else:
     r[13][2], r[13][14] = 1, 1
     r[14][1], r[14][7], r[14][13] = 1, 1, 1
     r[15][0], r[15][3], r[15][6], r[15][12], r[15][15] = 1, 1, 1, 1, 1
-
+ans = bin_pow(r, n, M)
+print(ans[e-1][e-1])
