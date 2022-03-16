64da97c81a7bea2bf116f7b879d19b57ce84517c
Shuhei Abe <blueletter123456789@gmail.com>
2022-03-16 09:16:53 +0900

    modify 073

--- 5_6/p073.py
+++ 5_6/p073.py
@@ -1,28 +1,13 @@
-def mul(n, r, M):
-    res = 1
-    p = 2
-    for i in range(n):
-        if r & 1 << i:
-            res *= p
-            res %= M
-        p *= p
-        p %= M
-    return res
-
+M = 1000000007
 n = int(input())
 l = list(map(int, input().split()))
-l.sort()
-M = 1000000007
 ans = 0
-r = [0]*n
+base = 1
 for i in range(n):
-    r[i] = 2 ** i
-    # r[i] = mul(n, i, M)
-
-for i in range(n):
-    # ans += l[i]%M * mul(n, i, M)
-    ans += l[i]%M * r[i]
+    ans += l[i]%M * base
     ans %= M
+    base *= 2
+    base %= M
 print(ans)
 
 # 全探索では
