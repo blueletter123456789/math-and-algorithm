8977d0af7c9f918e33bf9be10c0d8da0e6900217
Shuhei Abe <blueletter123456789@gmail.com>
2022-06-12 23:38:55 +0900

    create newton

---
+++ 4_3/newton2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	r := 2.0
+	a := 2.0
+
+	for i := 1; i <= 6; i++ {
+		zahyou_x := a
+		zahyou_y := a * a * a
+
+		sessen_a := 3 * a * a
+		sessen_b := zahyou_y - sessen_a * zahyou_x
+
+		next_a := (r - sessen_b) / sessen_a
+
+		fmt.Printf("Step %d %.15f -> %.15f\n", i, a, next_a)
+
+		a = next_a
+	}
+}
