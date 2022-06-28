3ecc6b507f0fda0711eeceb1c4ad084b574101cb
Shuhei Abe <blueletter123456789@gmail.com>
2022-06-29 08:14:56 +0900

    solved 059, 060

---
+++ 5_2/p059.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scan = bufio.NewScanner(os.Stdin)
+
+const (
+	initBufSize = 10000
+	maxBufSize = 10000000
+)
+
+func init() {
+	buf := make([]byte, initBufSize)
+	scan.Buffer(buf, maxBufSize)
+	scan.Split(bufio.ScanWords)
+}
+
+func main() {
+	lst := [4]int{6, 2, 4, 8}
+
+	n := NumStdin()
+
+	fmt.Println(lst[n%4])
+}
+
+func NumStdin() int {
+	scan.Scan()
+	num, err := strconv.Atoi(scan.Text())
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
---
+++ 5_2/p060.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scan = bufio.NewScanner(os.Stdin)
+
+const (
+	initBufSize = 10000
+	maxBufSize = 100000
+)
+
+func init() {
+	buf := make([]byte, initBufSize)
+	scan.Buffer(buf, maxBufSize)
+	scan.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := NumStdin()
+	if n % 4 != 0 {
+		fmt.Println("First")
+	} else {
+		fmt.Println("Second")
+	}
+}
+
+func NumStdin() int {
+	scan.Scan()
+	num, err := strconv.Atoi(scan.Text())
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
