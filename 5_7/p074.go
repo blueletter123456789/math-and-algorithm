72a6f69356aabee73db082b7c187b0d78a531adf
Shuhei Abe <blueletter123456789@gmail.com>
2022-07-12 09:22:58 +0900

    solved 074

---
+++ 5_7/p074.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scan = bufio.NewScanner(os.Stdin)
+
+const (
+	initBufSize = 10000
+	maxBufSize = 10000000
+)
+
+func init() {
+	buf := make([]byte, initBufSize)
+	scan.Buffer(buf, maxBufSize)
+	scan.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := NumStdin()
+	ans := 0
+	for i := 0; i < n; i++ {
+		num := NumStdin()
+		ans += (2 * (i+1) - n - 1) * num
+	}
+	fmt.Println(ans)
+}
+
+func NumStdin() int {
+	scan.Scan()
+	num, err := strconv.Atoi(scan.Text())
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
